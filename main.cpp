4cc428edd83ebe526e738ef87ca14722ebdb0216
Olaf <146707611+parowus1@users.noreply.github.com>
2023-10-21 00:24:56 +0200

    Dodanie menu drzewa binarnego

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <string>
 
+
 int main() {
     DrzewoBinarne tree;
     std::string input;
+    bool running = true;
+
+    while (running) {
+        std::cout << "Menu Binarnego Drzewa:\n";
+        std::cout << "1. Dodaj element\n";
+        std::cout << "2. Usuń element\n";
+        std::cout << "3. Usuń całe drzewo\n";
+        std::cout << "4. Szukaj elementu\n";
+        std::cout << "5. Wyświetl drzewo\n";
+        std::cout << "6. Zapisz drzewo do pliku\n";
+        std::cout << "7. Wczytaj drzewo z pliku\n";
+        std::cout << "8. Wczytaj plik tekstowy z cyfer\n";
+        std::cout << "9. Wyjście\n";
+        std::cout << "Wpisz swój wybór: ";
+        std::cin >> input;
 
     
 return 0;
