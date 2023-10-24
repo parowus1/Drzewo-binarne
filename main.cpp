7977337f5626766641e46e285f1941ee86854011
AdamZiolko <theadamzet@gmail.com>
2023-10-24 22:34:05 +0200

    Poprawa wczesniejszych bledow

--- main.cpp
+++ main.cpp
@@ -10,31 +10,31 @@ int main() {
     while (running) {
         std::cout << "Menu Binarnego Drzewa:\n";
         std::cout << "1. Dodaj element\n";
-        std::cout << "2. Usuń element\n";
-        std::cout << "3. Usuń całe drzewo\n";
+        std::cout << "2. Usun element\n";
+        std::cout << "3. Usun cale drzewo\n";
         std::cout << "4. Szukaj elementu\n";
-        std::cout << "5. Wyświetl drzewo\n";
+        std::cout << "5. Wyswietl drzewo\n";
         std::cout << "6. Narysuj drzewo\n";
         std::cout << "7. Zapisz drzewo do pliku\n";
         std::cout << "8. Wczytaj drzewo z pliku\n";
         std::cout << "9. Wczytaj plik tekstowy z cyfer\n";
-        std::cout << "0. Wyjście\n";
-        std::cout << "Wpisz swój wybór: ";
+        std::cout << "0. Wyjscie\n";
+        std::cout << "Wpisz swoj wybor: ";
         std::cin >> input;
 
         int value;
 
         if (input == "1") {
-            std::cout << "Wprowadź element do dodania: ";
+            std::cout << "Wprowadz element do dodania: ";
             std::cin >> value;
             //tree.Insert(value);
             } else if (input == "2") {
-            std::cout << "Wprowadź element do usunięcia: ";
+            std::cout << "Wprowadz element do usuniecia: ";
             std::cin >> value;
             //tree.Remove(value);
         } else if (input == "3") {
            // tree.Clear();
-            std::cout << "Drzewo usunięte.\n";
+            std::cout << "Drzewo usuniete.\n";
         } else {
             break;
         }
