43099b757a14c4faca30b3f31a9d5a0ce272061a
AdamZiolko <theadamzet@gmail.com>
2023-10-24 22:31:28 +0200

    Dodano kolejne funkcje

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 
 int main() {
-    DrzewoBinarne tree;
+    //DrzewoBinarne tree;
     std::string input;
     bool running = true;
 
@@ -14,10 +14,11 @@ int main() {
         std::cout << "3. Usuń całe drzewo\n";
         std::cout << "4. Szukaj elementu\n";
         std::cout << "5. Wyświetl drzewo\n";
-        std::cout << "6. Zapisz drzewo do pliku\n";
-        std::cout << "7. Wczytaj drzewo z pliku\n";
-        std::cout << "8. Wczytaj plik tekstowy z cyfer\n";
-        std::cout << "9. Wyjście\n";
+        std::cout << "6. Narysuj drzewo\n";
+        std::cout << "7. Zapisz drzewo do pliku\n";
+        std::cout << "8. Wczytaj drzewo z pliku\n";
+        std::cout << "9. Wczytaj plik tekstowy z cyfer\n";
+        std::cout << "0. Wyjście\n";
         std::cout << "Wpisz swój wybór: ";
         std::cin >> input;
 
@@ -26,15 +27,18 @@ int main() {
         if (input == "1") {
             std::cout << "Wprowadź element do dodania: ";
             std::cin >> value;
-            tree.Insert(value);
+            //tree.Insert(value);
             } else if (input == "2") {
             std::cout << "Wprowadź element do usunięcia: ";
             std::cin >> value;
-            tree.Remove(value);
+            //tree.Remove(value);
         } else if (input == "3") {
-            tree.Clear();
+           // tree.Clear();
             std::cout << "Drzewo usunięte.\n";
-        }//////
+        } else {
+            break;
+        }
         
+    }
 return 0;
 }
