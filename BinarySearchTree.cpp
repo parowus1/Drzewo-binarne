12232b43cde2b6ad3057bf14477358b6f9d5a43d
Olaf <146707611+parowus1@users.noreply.github.com>
2023-10-25 02:20:20 +0200

    Przeszukiwanie drzewa

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -3,18 +3,33 @@
 
 BinarySearchTree::BinarySearchTree() : root(nullptr) {}
 
+//czyszczenie
 BinarySearchTree::~BinarySearchTree() {
     Clear();
 }
-
+//dodanie
 void BinarySearchTree::Insert(int value) {
     InsertRecursive(root, value);
 }
-
+//usunięcie
 bool BinarySearchTree::Remove(int value) {
     return RemoveRecursive(root, value);
 }
 
 void BinarySearchTree::Clear() {
     ClearRecursive(root);
+}
+//przeszukiwanie drzewa
+bool BinarySearchTree::Search(int value) const {
+    Node* current = root;
+    while (current != nullptr) {
+        if (current->data == value) {
+            return true;
+        } else if (value < current->data) {
+            current = current->left;
+        } else {
+            current = current->right;
+        }
+    }
+    return false;
 }
