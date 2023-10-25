7dd23679e0b99faa0a140693e2daae8e350a03f4
Olaf <146707611+parowus1@users.noreply.github.com>
2023-10-25 13:23:11 +0200

    Dodanie komentarzy

--- main.cpp
+++ main.cpp
@@ -27,30 +27,30 @@ int main() {
 
         if (input == "1") {
             std::cout << "Wprowadz element do dodania: ";
-            std::cin >> value;
-            if(value[0] <= '9' && value[0] >='0'){
-                tree.Insert(std::stoi(value));
+            std::cin >> value; //wprowadzanie liczby przez uzytkownika
+            if(value[0] <= '9' && value[0] >='0'){  //program sprawdza, czy jest to liczba lub nie
+                tree.Insert(std::stoi(value)); //Jeśli wprowadzona wartość jest liczbą (od 0 do 9), program przekształci wartość w liczbe całkowitą i umieści ją w drzewie
             } else {
-                std::cout<< "\n!!! To nie jest liczba !!!\n\n";
+                std::cout<< "\n!!! To nie jest liczba !!!\n\n"; //jesli wartosc nie jest liczba wyswietli komunikat i nie doda do drzewa
             }
             
             } else if (input == "2") {
             std::cout << "Wprowadz element do usuniecia: ";
-            std::cin >> value;
-            if(value[0] <= '9' && value[0] >='0'){
-               tree.Remove(std::stoi(value));
+            std::cin >> value; //wprowadzanie liczby przez uzytkownika
+            if(value[0] <= '9' && value[0] >='0'){  //program sprawdza, czy jest to liczba lub nie
+               tree.Remove(std::stoi(value));    //usuwa liczbe jesli wartość jej jest poprawna
             } else {
                 std::cout<< "\n!!! To nie jest liczba !!!\n\n";
             }
         } else if (input == "3") {
-            tree.Clear();
+            tree.Clear();   
             std::cout << "Drzewo usuniete.\n";
+            
         } else if (input == "4") {
             std::cout << "Element do wyszukania: ";
-            std::cin >> value;
-
-             if(value[0] <= '9' && value[0] >='0'){
-                std::cout << (tree.Search(std::stoi(value)) ? "\nZnaleziono element \n" : "\nNie znaleziono elementu\n");
+            std::cin >> value; //wprowadzanie liczby przez uzytkownika
+             if(value[0] <= '9' && value[0] >='0'){  //program sprawdza, czy jest to liczba lub nie
+                std::cout << (tree.Search(std::stoi(value)) ? "\nZnaleziono element \n" : "\nNie znaleziono elementu\n"); //szuka elementu w drzewie 
             } else {
                 std::cout<< "\n!!! To nie jest liczba !!!\n\n";
             }
@@ -59,6 +59,7 @@ int main() {
             std::cout << "Tree: ";
             tree.Display();
             std::cout << "\n";
+            
         } else if (input == "6") {
             tree.DisplayTree();
         
