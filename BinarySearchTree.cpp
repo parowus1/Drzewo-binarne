99b92a6b2db630539113f912c2950475c78426f3
Olaf <146707611+parowus1@users.noreply.github.com>
2023-10-25 03:05:52 +0200

    Binarne dodawanie i usuwanie

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -35,7 +35,48 @@ void BinarySearchTree::Display() const {
     InOrderDisplay(root);
     std::cout << std::endl;
 }
-
+//binarne zapisywanie
+        void BinarySearchTree::InsertRecursive(Node*& node, int value) {
+    if (node == nullptr) {
+        node = new Node(value);
+    } else if (value < node->data) {
+        InsertRecursive(node->left, value);
+    } else {
+        InsertRecursive(node->right, value);
+    }
+}
+// binarne usuwanie
+bool BinarySearchTree::RemoveRecursive(Node*& node, int value) {
+    if (node == nullptr) {
+        return false;
+    }
+    if (value < node->data) {
+        return RemoveRecursive(node->left, value);
+    } else if (value > node->data) {
+        return RemoveRecursive(node->right, value);
+    } else {
+        if (node->left == nullptr && node->right == nullptr) {
+            delete node;
+            node = nullptr;
+        } else if (node->left == nullptr) {
+            Node* temp = node;
+            node = node->right;
+            delete temp;
+        } else if (node->right == nullptr) {
+            Node* temp = node;
+            node = node->left;
+            delete temp;
+        } else {
+            Node* minRight = node->right;
+            while (minRight->left != nullptr) {
+                minRight = minRight->left;
+            }
+            node->data = minRight->data;
+            RemoveRecursive(node->right, minRight->data);
+        }
+        return true;
+    }
+}
 
         
     }
